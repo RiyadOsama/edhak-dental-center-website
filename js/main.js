e738d2d59208acaef48e32c0bb3582f83e3b0522
RiyadOsama <riyadosama14112000@gmail.com>
2025-08-23 23:07:02 +0300

    ✨ feat:  Added The Patients Section

--- js/main.js
+++ js/main.js
@@ -17,3 +17,33 @@ fetch("_footer.html")
       window.scrollTo({ top: 0, behavior: "smooth" });
     };
   });
+
+// Owl Carousel
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 4,
+  loop: true,
+  margin: 30,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [1000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
